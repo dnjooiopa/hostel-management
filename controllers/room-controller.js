f8e557565dec87f7463dc12923b4fb21a9e653fd
dnjooiopa <dnjooiopa@gmail.com>
2021-01-30 19:27:54 +0700

    :hammer: Change get room by type_id -> hotel_id

--- controllers/room-controller.js
+++ controllers/room-controller.js
@@ -4,27 +4,22 @@ const { status, successMessage, errorMessage } = require('../helpers/status')
 const { isEmpty } = require('../helpers/validation')
 
 const getRooms = async (req, res) => {
-    const { type_id } = req.query
+    const { hotel_id } = req.query
 
-    if (isEmpty(type_id)) {
-        errorMessage.error = 'Type id cannot be empty'
+    if (isEmpty(hotel_id)) {
+        errorMessage.error = 'Please provide hotel_id'
         res.status(status.bad).send(errorMessage)
     }
 
     const getRoomQuery = `
     SELECT * FROM room
-    WHERE type_id = $1
+    WHERE hotel_id = $1
     `
-    const values = [type_id]
+    const values = [hotel_id]
 
     try {
         const { rows } = await db.query(getRoomQuery, values)
         const dbResults = rows
-        if (dbResults.length == 0) {
-            errorMessage.error = 'This room type has no room'
-            return res.status(status.notfound).send(errorMessage)
-        }
-
         successMessage.data = dbResults
         return res.status(status.success).send(successMessage)
     } catch (error) {
