d25684e1a07bb225e073275f20403661371d4d1e
dnjooiopa <dnjooiopa@gmail.com>
2021-01-30 18:53:51 +0700

    :x: Remove room type feature

--- routes/room-type-route.js
+++
@@ -1,7 +0,0 @@
-const { getRoomTypes } = require('../controllers/room-type-controller')
-
-const router = require('express').Router()
-
-router.get('/room-types', getRoomTypes)
-
-module.exports = router
