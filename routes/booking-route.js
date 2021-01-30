f9ba68eb2db24f00537c1082fa49bcf581975ace
dnjooiopa <dnjooiopa@gmail.com>
2021-01-30 19:40:18 +0700

    :rocket: Feat create booking

--- routes/booking-route.js
+++ routes/booking-route.js
@@ -1,8 +1,9 @@
-const { getBookings } = require('../controllers/booking-controller')
+const { getBookings, createBooking } = require('../controllers/booking-controller')
 const { verifyToken } = require('../middlewares/verify-token')
 
 const router = require('express').Router()
 
 router.get('/bookings', verifyToken, getBookings)
+router.post('/bookings', verifyToken, createBooking)
 
 module.exports = router
