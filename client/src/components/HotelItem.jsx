1e46ad845e15b1f580ef1296fc6685d099b77551
dnjooiopa <dnjooiopa@gmail.com>
2021-01-31 21:07:34 +0700

    :repeat: Add start price to hotel item

--- client/src/components/HotelItem.jsx
+++ client/src/components/HotelItem.jsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export const HotelItem = ({ hotel }) => {
+  const [startPrice, setStartPrice] = useState(0.0)
+
+  useEffect(() => {
+    const prices = hotel.rooms.map((room) => Number.parseFloat(room.price))
+    setStartPrice(Math.min(...prices).toFixed(2))
+  }, [])
+
   return (
     <div className="rounded m-2 p-6">
       <div className="text-lg">{hotel.name}</div>
       <div>{hotel.city}</div>
+      <div>start from THB{startPrice}/night</div>
       <Link to={`/hotels/${hotel.hotel_id}`}>
         <button type="text">View Detail</button>
       </Link>
