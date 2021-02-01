a1c243d5c6d1f06f22f271502fc0c98af24c4ee9
dnjooiopa <dnjooiopa@gmail.com>
2021-02-01 18:07:39 +0700

    :rocket: Feat insert hotel

--- controllers/hotel-controller.js
+++ controllers/hotel-controller.js
@@ -1,6 +1,6 @@
 const db = require('../db')
 
-const { isEmpty } = require('../helpers/validation')
+const { isEmpty, generateID } = require('../helpers/validation')
 const { status, successMessage, errorMessage } = require('../helpers/status')
 const getFacility = require('./facility-controller')
 
@@ -62,17 +62,64 @@ const getHotelData = async (req, res) => {
         }
 
         const { rows: rooms } = await db.query(`SELECT * FROM room WHERE hotel_id=$1`, [hotel_id])
-        
+
         successMessage.data = { ...dbResult, facility, rooms }
         return res.status(status.success).send(successMessage)
     } catch (error) {
         errorMessage.error = 'Operation was not successful'
         return res.status(status.error).send(errorMessage)
     }
+}
 
+const insertHotel = async (req, res) => {
+    const { name, detail, phone, city, latitude, longitude, facility, rooms } = req.body
+    const insertHotelQuery = `
+        INSERT INTO hotel
+        (hotel_id, name, detail, phone, city, latitude, longitude, document)
+        VALUES
+        ($1, $2, $3, $4, $5, $6, $7, to_tsvector($8 || ' ' || $9 || ' ' || $10))
+        RETURNING *
+        `
+    const hotel_id = generateID()
+    const hotelValues = [hotel_id, name, detail, phone, city, latitude, longitude, name, detail, city]
+
+    const { air_condition, restaurant, wifi, parking, swimming_pool, lift } = facility
+    const insertFacilityQuery = `
+        INSERT INTO facility
+        (hotel_id,  air_condition, restaurant, wifi, parking, swimming_pool, lift )
+        VALUES
+        ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING *
+        `
+    const facilityValues = [hotel_id, air_condition, restaurant, wifi, parking, swimming_pool, lift]
+
+    try {
+        await db.query(insertHotelQuery, hotelValues)
+        await db.query(insertFacilityQuery, facilityValues)
+
+        for (let room of rooms) {
+            const { room_type, max_guest, price } = room
+            const insertRoomQuery = `
+                INSERT INTO room
+                (hotel_id, room_id, room_type, max_guest, price)
+                VALUES
+                ($1, $2, $3, $4, $5)
+                RETURNING *
+                `
+            const values = [hotel_id, generateID(), room_type, max_guest, price]
+            await db.query(insertRoomQuery, values)
+        }
+
+        successMessage.data = { hotel_id }
+        return res.status(status.created).send(successMessage)
+    } catch (err) {
+        errorMessage.error = 'Operation was not successful'
+        return res.status(status.error).send(errorMessage)
+    }
 }
 
 module.exports = {
     searchHotel,
-    getHotelData
+    getHotelData,
+    insertHotel
 }
--- routes/hotel-route.js
+++ routes/hotel-route.js
@@ -1,8 +1,9 @@
-const { searchHotel, getHotelData } = require('../controllers/hotel-controller')
+const { searchHotel, getHotelData, insertHotel } = require('../controllers/hotel-controller')
 
 const router = require('express').Router()
 
 router.get('/hotels', searchHotel)
 router.get('/hotels/:hotel_id', getHotelData)
+router.post('/hotels', insertHotel)
 
 module.exports = router
