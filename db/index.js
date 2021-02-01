326347b6e5da4f1a09516dfba2ad3186e16e75a8
dnjooiopa <dnjooiopa@gmail.com>
2021-02-01 18:47:00 +0700

    :repeat: Enable SSL

--- db/index.js
+++ db/index.js
@@ -4,12 +4,12 @@ require("dotenv").config()
 const isProduction = process.env.NODE_ENV === 'production'
 
 const pool = new Pool(
-    isProduction ? { connectionString: process.env.DATABASE_URL } : {
+    isProduction ? { connectionString: process.env.DATABASE_URL, ssl: true } : {
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
         database: process.env.DB_DATABASE,
         password: process.env.DB_NAME,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
     }
 )
 
